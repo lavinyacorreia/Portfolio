5fe7911b5a21ef4ce486cf49c3c62c7553ca75e1
Lavinya Correia <lavinya.correia01@gmail.com>
2021-05-16 11:12:30 -0300

    Last commit

--- src/app.js
+++ src/app.js
@@ -44,10 +44,22 @@ const highlightMenu = () => {
         return
     }
 
-    if((elem && window.innerWidth < 960 && scrollPos < 600) || elem){
+    if((elem && window.innerWidth < 900 && scrollPos < 600) || elem){
         elem.classList.remove('highlight')
     }
 };
 
 window.addEventListener('scroll', highlightMenu);
 window.addEventListener('click', highlightMenu);
+
+//Close mobile menu when clickengo on a menu item
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active')
+    if(window.innerWidth <= 768 && menuBars){
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+};
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
+
